7b81c835c103e5d5a65a8cba14ea046252a5a724
yerimgim <imyrgim92@gmail.com>
2026-09-08 13:16:32 +0900

    feat: 기본 type 설정

---
+++ src/types/task.ts
@@ -0,0 +1,10 @@
+export type Priority = "low" | "medium" | "high";
+export type FilterStatus = "all" | "active" | "completed";
+
+export interface Task {
+  id: string;
+  title: string;
+  completed: boolean;
+  priority: Priority;
+  createdAt: number;
+}
